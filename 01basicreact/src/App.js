f047b2eb0fdccd808fea1da94b6ed1672504225c
Shalini Budholiya <shalinibudholiya46@gmail.com>
2024-10-05 16:27:14 +0530

    React project-1 with createreact.

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+      <h1>Hello World! | Shalini Budholiya</h1>
+      <Chai />
+
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+
+    <>
+      <h1>Chai aur React with Vite | Shalini Budholiya</h1>
+      <Chai />
+    </>
+
+  )
+}
+
+export default App
