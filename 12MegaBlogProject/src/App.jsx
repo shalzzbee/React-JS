6474aee68daf84a0aa1137bfca76944ee6b50eaa
Shalini Budholiya <shalinibudholiya46@gmail.com>
2024-11-21 12:54:42 +0530

    Mega Project of Complex Blog

---
+++ 12MegaBlogProject/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import './App.css'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  //console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+
+      <div className='w-full block'>
+        <Header />
+
+        <main>
+          TODO:  <Outlet />
+        </main>
+
+        <Footer />
+
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
